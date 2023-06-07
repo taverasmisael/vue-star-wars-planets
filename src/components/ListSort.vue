4db0a9b783eca175672dad7349e819cd422df32f
Misael Taveras <hello@taverasmisael.com>
2023-06-07 09:15:53 -0400

    feat: add styles for the filters

--- src/components/ListSort.vue
+++ src/components/ListSort.vue
@@ -7,15 +7,19 @@ const { sort } = useStore();
 const { order, key } = sort;
 </script>
 <template>
-  <div>
-    <label for="sort">Sort by:</label>
-    <select id="sort" v-model="key">
-      <option v-for="sort in SortingKeys" :value="sort" :key="sort">{{ headerCase(sort) }}</option>
-    </select>
+  <div class="container mx-auto flex items-center gap-8 px-8 text-neutral-800 md:justify-end md:pr-0">
+    <div>
+      <label for="sort" class="mb-2 inline-block">Sort by</label>
+      <select id="sort" v-model="key">
+        <option v-for="sort in SortingKeys" :value="sort" :key="sort">{{ headerCase(sort) }}</option>
+      </select>
+    </div>
 
-    <label for="order">Order:</label>
-    <select id="order" v-model="order">
-      <option v-for="order in SortingOrders" :value="order" :key="order">{{ constantCase(order) }}</option>
-    </select>
+    <div>
+      <label for="order" class="mb-2 inline-block">Order</label>
+      <select id="order" v-model="order">
+        <option v-for="order in SortingOrders" :value="order" :key="order">{{ constantCase(order) }}</option>
+      </select>
+    </div>
   </div>
 </template>
